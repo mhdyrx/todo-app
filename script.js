0e5962dccabd28912f8a8fd914444692b2a5f0fa
mhdyr <mhdyrghx@gmail.com>
2026-07-01 13:52:03 +0330

    edit behavior buttons added

--- script.js
+++ script.js
@@ -27,6 +27,14 @@ const createMarkup = function (value, taskStatus) {
         <input type="checkbox" class="task-check" ${taskStatus === "checked" ? "checked" : ""}/>
 
         <p class="task-title">${value}</p>
+
+        <div class="task-btns">
+        <button type="button" class="edit-btn">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+          </svg>
+        </button>
+
         <button type="button" class="delete-btn">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -41,6 +49,7 @@ const createMarkup = function (value, taskStatus) {
             />
           </svg>
         </button>
+        </div>
       </li>
   `;
   return markup;
@@ -90,6 +99,11 @@ const deleteTask = function (task) {
 };
 
 addBtn.addEventListener("click", addTask);
+taskInput.addEventListener("keydown", function (e) {
+  if (e.key !== "Enter") return;
+  addTask();
+});
+
 taskList.addEventListener("click", function (e) {
   const btn = e.target.closest(".delete-btn");
   const check = e.target.closest(".task-check");
